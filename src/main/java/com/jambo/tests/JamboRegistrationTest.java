677234dc2b7f10c455131ea497d9b21026a00e7d
collins <collinskipkoech95@gmail.com>
2023-01-25 10:38:26 +0300

    final commit

--- src/main/java/com/jambo/tests/JamboRegistrationTest.java
+++ src/main/java/com/jambo/tests/JamboRegistrationTest.java
@@ -1,17 +1,17 @@
-package com.jambo.tests;
-
-import com.jambo.pages.JamboRegistration;
-import com.jambo.utility.TestBase;
-import org.junit.jupiter.api.Test;
-
-public class JamboRegistrationTest extends TestBase {
-
-    JamboRegistration jamboRegistration;
-
-    @Test
-    public void testRegistration(){
-        jamboRegistration = new JamboRegistration();
-        jamboRegistration.goTo();
-        //jamboRegistration.register();
-    }
-}
+//package com.jambo.tests;
+//
+//import com.jambo.pages.JamboRegistration;
+//import com.jambo.utility.TestBase;
+//import org.junit.jupiter.api.Test;
+//
+//public class JamboRegistrationTest extends TestBase {
+//
+//    JamboRegistration jamboRegistration;
+//
+//    @Test
+//    public void testRegistration(){
+//        jamboRegistration = new JamboRegistration();
+//        jamboRegistration.goTo();
+//        //jamboRegistration.register();
+//    }
+//}
